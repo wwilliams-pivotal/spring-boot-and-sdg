9974f30d709a585a1e7a64b5a404310707765e6a
John Blum <jblum@pivotal.io>
2017-10-31 10:02:38 -0700

    Optimize imports.

--- BootWithAnnotations/src/main/java/io/pivotal/app/test/TestGemFireTemplate.java
+++ BootWithAnnotations/src/main/java/io/pivotal/app/test/TestGemFireTemplate.java
@@ -1,22 +1,21 @@
 package io.pivotal.app.test;
 
-import io.pivotal.domain.Employee;
-
 import java.util.Collection;
 
+import org.apache.geode.cache.Region;
+import org.apache.geode.cache.query.SelectResults;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.gemfire.GemfireTemplate;
 
-import org.apache.geode.cache.Region;
-import org.apache.geode.cache.query.SelectResults;
+import io.pivotal.domain.Employee;
 
-public class TestGemFireTemplate 
+public class TestGemFireTemplate
 {
 
 	private ConfigurableApplicationContext ctx = null;
-	
-	public void init() 
+
+	public void init()
 	{
 		ctx = new ClassPathXmlApplicationContext("config/application-context.xml");
 	}
@@ -25,40 +24,40 @@ public class TestGemFireTemplate
 	{
 		@SuppressWarnings("unchecked")
 		GemfireTemplate empTemplate = new GemfireTemplate((Region<Integer, Employee>) ctx.getBean("employees"));
-		
+
 		System.out.println("-> template.query() test \n ");
-		
+
 		SelectResults<?> results = empTemplate.query("deptno = 40");
 		@SuppressWarnings("unchecked")
 		Collection<Employee> emps = (Collection<Employee>) results.asList();
-		
-		
+
+
 		for (Employee e: emps)
 		{
 			System.out.println(e.toString());
 		}
-		
+
 		System.out.println("\n-> template.get(key) test \n ");
-		
+
 		Employee emp = empTemplate.get("7373");
-		
+
 		System.out.println(emp.toString());
-		
+
 		System.out.println("\n-> template.find() test \n ");
-		
-		SelectResults<Employee> clerkEmpResults = 
+
+		SelectResults<Employee> clerkEmpResults =
 				empTemplate.find("SELECT * from /employees WHERE job=$1", "CLERK");
-		
+
 		Collection<Employee> clerkEmps = (Collection<Employee>) clerkEmpResults.asList();
-		
+
 		for (Employee e: clerkEmps)
 		{
 			System.out.println(e.toString());
 		}
-		
+
 	}
-	
-	public static void main(String[] args) 
+
+	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
 		TestGemFireTemplate test = new TestGemFireTemplate();
@@ -66,6 +65,6 @@ public class TestGemFireTemplate
 		test.init();
 		test.run();
 		System.out.println("\nAll done.... ");
-		
+
 	}
 }
